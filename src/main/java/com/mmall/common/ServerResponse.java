9d5b89d97252af22cb161d42d60ad5e03ebf3ced
github-yinrui <997202844@qq.com>
2018-07-02 21:40:07 +0800

    3333

--- src/main/java/com/mmall/common/ServerResponse.java
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -1,7 +1,11 @@
 package com.mmall.common;
 
-import java.io.Serializable;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
+import java.io.Serializable;
+//value为null时，key不会显示
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class ServerResponse<T> implements Serializable {
     private int status;
     private String msg;
@@ -22,6 +26,7 @@ public class ServerResponse<T> implements Serializable {
         this.status = status;
         this.msg = msg;
     }
+    @JsonIgnore
     public boolean isSuccess(){
         return  this.status == ResponseCode.SUCCESS.getCode();
     }
@@ -38,10 +43,25 @@ public class ServerResponse<T> implements Serializable {
     public  static <T> ServerResponse<T> createBySuccess(){
         return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
     }
+    public  static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    }
+    public  static <T> ServerResponse<T> createBySuccess(String msg,T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    }
+    public  static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    }
 
-
-
-
+    public  static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    }
+    public  static <T> ServerResponse<T> createByErrorMessage(String errorMessage){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMessage);
+    }
+    public  static <T> ServerResponse<T> createByErrorMessage(int errorCode,String errorMessage){
+        return new ServerResponse<T>(errorCode,errorMessage);
+    }
 
 
 }
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -1,10 +1,21 @@
 package com.mmall.service.impl;
 
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.UserMapper;
 import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImpl implements IUserService {
+    @Autowired
+    private UserMapper userMapper;
     @Override
     public Object login(String username, String password) {
-        return null;
+        int resultcount = userMapper.checkUsername(username);
+        if(resultcount == 0){
+            return ServerResponse.createByErrorMessage("用户名不存在");
+        }
+        else{
+            return ServerResponse.createBySuccessMessage("登陆成功");
+        }
     }
 }
